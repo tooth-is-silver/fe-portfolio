ac109691f4dfd2d91c9ebdb3e698b2a7027a733c
Ga eun Lee <35989487+tooth-is-silver@users.noreply.github.com>
2026-03-19 10:36:03 +0900

    refactor: 기존 컴포넌트 전체 제거 - 바다 콘셉트 리빌드 준비 (#41)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import './App.css';
-
 function App() {
   return (
-    <>
-      <Header />
-
-      <main>
-        <Hero />
-        <Skills />
-        <Projects />
-        <Contact />
-      </main>
-
-      <Footer />
-    </>
-  );
+    <div id="root-canvas" />
+  )
 }
 
-export default App;
+export default App
